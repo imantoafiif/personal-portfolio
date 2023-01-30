b2e5b636cf1dad1e029aec059d877365a8e91103
imantoafiif@gmail.com <imantoafiif@gmail.com>
2023-01-30 22:34:44 +0700

    projects

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,7 +1,23 @@
+import Banner from "../../components/Banner";
+import Navbar from "../../components/Navbar";
+import style from './Projects.module.css'
+
 const Project = () => {
 
     return (
-        <></>
+        <>
+            <Navbar></Navbar>
+            <Banner>
+                <>
+                    <div className={style.header}>
+                        <h1 className={style.title}>Projects</h1>
+                        <p className={style.subtitle}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam lorem ipsum dolor sit amet, consectetur
+                        </p>
+                    </div>
+                </>
+            </Banner>
+        </>
     )
 
 }
