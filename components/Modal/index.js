6be535143fc56181cc16931d2af77194059863ba
imantoafiif@gmail.com <imantoafiif@gmail.com>
2023-02-09 23:12:30 +0700

    update

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -54,7 +54,7 @@ const Modal = ({ active, onClose }) => {
                             className={style.screenshot}
                             src='https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80'>
                         </img>
-                        <p>explain screenshot 2</p>
+                        <p>explain screenshot 23</p>
                     </div>
                     
                 </div>
