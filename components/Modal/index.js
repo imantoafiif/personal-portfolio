197cf8625bb60628a60a76c256e844fcbb9a2b2a
imantoafiif@gmail.com <imantoafiif@gmail.com>
2023-02-09 15:19:51 +0700

    update

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -14,7 +14,6 @@ const Modal = ({ active }) => {
 
     return ( 
         <div
-            onClick={}
             id="modal-container"    
             className={style.background}>
             
