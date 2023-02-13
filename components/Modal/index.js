4375040f44f1959021e577eb7e11caf53512a22f
imantoafiif@gmail.com <imantoafiif@gmail.com>
2023-02-13 22:45:01 +0700

    modal rework

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -40,39 +40,35 @@ const Modal = ({ active, onClose }) => {
                     </p>
                 </div>
                 <div className={style.modal_body}>
-                    {
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0 ,0, 0, 0,0 ,0, 0,0, 0, 0,0, 0,0, 0].map(item => (
-                            <p>abalsk</p>
-                        ))
-                    }
+                    <div className={style.modal_content}>
+                        {
+                            // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0 ,0, 0, 0,0 ,0, 0,0, 0, 0,0, 0,0, 0].map(item => (
+                            //     <p>abalsk</p>
+                            // ))
+                        }
+                        <p className={style.description}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam lorem ipsum dolor sit amet, consectetur
+                    </p>
+                    <div>
+                        <img 
+                            className={style.screenshot}
+                            src='https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg'>
+                        </img>
+                        <p>explain screenshot 1</p>
+                        <img 
+                            className={style.screenshot}
+                            src='https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80'>
+                        </img>
+                        <p>explain screenshot 2</p>
+                    </div>
+                    </div>
+                    
                 </div>
-                {/* <div
-                    onClick={a}
-                    className={style.modal}>
-                    <div className={style.header}>
-                        <h3 className={style.header_title}>
-                            Judul Container
-                        </h3>
-                        <p 
-                            onClick={onClose}
-                            className={style.header_abort}>
-                            <FontAwesomeIcon
-                                icon={faClose}
-                            />
-                        </p>
-                    </div>
-                    <div className={style.modal_body}>
-                        <div className={style.modal_body_container}>
-                            {
-                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map(item => (
-                                    <p className={style.description}>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam lorem ipsum dolor sit amet, consectetur
-                                    </p>
-                                ))
-                            }
-                        </div>
-                    </div>
-                </div> */}
+                <div className={style.footer}>
+                    <button className={style.footer_dismiss}>
+                        OK
+                    </button>
+                </div>
             </div>
         </div>
      );
