2d8ea0498bf96343ada56015d48b7cf0924f92c3
Afiif Imanto <imantoafiif@gmail.com>
2023-04-09 12:19:04 +0700

    udpate

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* <meta name="theme-color" content="#2481cc" /> */}
+        <title>Afiif Imanto - Digital Resume</title>
         <meta name="viewport" content="width=device-width, user-scalable=no" />
       </Head>
       <body>
