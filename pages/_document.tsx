3653081504a95c9769476d04cf7357402e56fd6c
imantoafiif@gmail.com <imantoafiif@gmail.com>
2023-04-10 22:57:26 +0700

    change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, user-scalable=no" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />      
       </Head>
       <body>
         <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,26 @@ const index = () => {
     const [id, setId] = useState<number>(0)
     const [write, setWrite] = useState<boolean>(true)
 
+    const work = [
+        { title: 'Front-end Developer', icon: 'fas fa-laptop', desc: 'The new common language will be more simple and regular than. It will be more Occidental in fact' },
+        { title: 'Back-end Developer', icon: 'fas fa-database', desc: 'The new common language will be more simple and regular than. It will be more Occidental in fact' },
+        { title: 'Mobile App Developer', icon: 'fas fa-mobile-phone', desc: 'The new common language will be more simple and regular than. It will be more Occidental in fact' },
+
+    ]
     const roles = [
         'developer',
         'Front-end',
         'Back-end',
-        'full stack'
+        'full stack',
+        'freelancer',
     ]
 
     useEffect(() => {
-        const seed = Math.floor(50 + Math.random() * 400)
-        const timer = setInterval(type, (write ? seed : 30))
+        const seed = Math.floor(0 + Math.random() * 600)
+        const timer = setInterval(type, (write ? seed : 20))
         setTimeout(() => {
             clearInterval(timer);
-        }, (write ? seed : 30));
+        }, (write ? seed : 20));
     }, [role, write, id])
 
     const type = () => {
@@ -46,25 +53,121 @@ const index = () => {
             <div className={style.hero_container}>
                 <video autoPlay loop muted className={style.bg_vid}>
                     <source 
-                        src="/109484315_main_xxl.mp4" 
+                        src="https://videos.pond5.com/beautiful-aerial-jakarta-skyline-night-footage-109484315_main_xxl.mp4" 
                         type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
                 <div className={style.container_overlay}>
                     <div className={style.internal_container}>
+                        {/* <img
+                        className={style.avatar} 
+                        src='https://media.licdn.com/dms/image/D5603AQFBH48M5DNceQ/profile-displayphoto-shrink_400_400/0/1671704555138?e=1686787200&v=beta&t=XtPBYIMutbgdMEv4mgE84kZnpT7k7XGhMuOv6YBFM_g'></img> */}
                         <div className={style.status}>
                             <p className={style.title}>hi, i'm <strong>afiif imanto</strong>
                             </p>
-                            <h2>a { role }<span className={style.blip}>|</span></h2>
-                            <p className={style.subtitle}>based in Jakarta, Indonesia</p>
+                            <h2>{ role }<span className={style.blip}>|</span></h2>
+                            <p className={style.subtitle}>based in Jakarta, Indonesia 🇮🇩</p>
                             <button className={style.button}>Download CV</button>
                         </div>
-                        <img
-                            className={style.avatar} 
-                            src='https://media.licdn.com/dms/image/D5603AQFBH48M5DNceQ/profile-displayphoto-shrink_400_400/0/1671704555138?e=1686787200&v=beta&t=XtPBYIMutbgdMEv4mgE84kZnpT7k7XGhMuOv6YBFM_g'></img>
+                            <img
+                                className={style.avatar} 
+                                src='https://media.licdn.com/dms/image/D5603AQFBH48M5DNceQ/profile-displayphoto-shrink_400_400/0/1671704555138?e=1686787200&v=beta&t=XtPBYIMutbgdMEv4mgE84kZnpT7k7XGhMuOv6YBFM_g'></img>
                     </div>
                 </div>
             </div>
+            <div className={style.content}>
+                <div className={style.content_container}>
+                    <h2>about me</h2>
+                    <p className={style.sub}>Let's get to know a little bit about my proffesional side</p>
+                    <p>Web Bean has everything you need to get your new website up and running in no time! All of the templates and themes on Web Bean w3c valid error free code and easy to use. Web Bean has everything you'll need to get your new website up and running in no time! All of the new templates and themes on Web Bean w3c valid error free code and easy to use. Web Bean has everything you need to get your new website up and running in no time! Web Bean w3c valid error free code and easy to use. All of the templates and themes on Web Bean w3c valid error free code and easy to use. Web Bean w3c valid error free code and easy to use. All of the error free code and easy to use.</p>
+                    {/* <p>Indonesian based hustler, tech enthusiast, learner by heart  and a web developer with 4+ years of experience  specializing in front end development with solid  knowledge in front end stacks such as React.js, Vue.js, TypeScript, CSS, Boostrap and MUI, with hands on experience in developing user centric systems for companies in various industries</p> */}
+                    <div className={style.socmeds}>
+                        <img src='gmail.png'></img>
+                        <img src='github-logo.png'></img>
+                        <img src='instagram.png'></img>
+                        <img src='linkedin.png'></img>
+                        <img src='telegram.png'></img>
+                    </div>
+                </div>
+            </div>
+            <div className={style.content}>
+                <div className={style.content_container}>
+                    <h2>what i do</h2>
+                    <p className={style.sub}>How can i help with your next project ?</p>
+                    <div className={style.activity_content}>
+                        {
+                            [0, 0, 0, 0].map(item => (
+                                <div className={style.work_container}>
+                                    <div className={style.work_item_head}>
+                                        <i className='fas fa-laptop'></i>
+                                    </div>
+                                    <div className={style.work_item_content}>
+                                        <div className={style.bg_interactive}></div>
+                                        <h4><label>Web Design</label></h4>
+                                        <p><label>The new common language will be more simple and regular than. It will be more Occidental in fact.</label></p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                        
+                    </div>
+                </div>
+            </div>
+            <div className={style.content}>
+                <div className={style.content_container}>
+                    <h2>resume</h2>
+                    <p className={style.sub}>A summary of my resume</p>
+                    <div className={style.summary_container}>
+                        <div className={style.summary}>
+                            <h2>My Education</h2>
+                            <div className={style.summary_items}>
+                                <div className={style.summary_items_container}>
+                                    <h2>Master in Computer Engineering</h2>
+                                    <p>Harvard University / 2015 - 2017</p>
+                                    <p className={style.summary_desc}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy text.</p>
+                                </div>
+                                <div className={style.summary_items_container}>
+                                    <h2>Master in Computer Engineering</h2>
+                                    <p>Harvard University / 2015 - 2017</p>
+                                    <p className={style.summary_desc}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy text.</p>
+                                </div>
+                                <div className={style.summary_items_container}>
+                                    <h2>Master in Computer Engineering</h2>
+                                    <p>Harvard University / 2015 - 2017</p>
+                                    <p className={style.summary_desc}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy text.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={style.summary}>
+                            <h2>My Experience</h2>
+                            <div className={style.summary_items}>
+                                <div className={style.summary_items_container}>
+                                    <h2>Master in Computer Engineering</h2>
+                                    <p>Harvard University / 2015 - 2017</p>
+                                    <p className={style.summary_desc}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy text.</p>
+                                </div>
+                                <div className={style.summary_items_container}>
+                                    <h2>Master in Computer Engineering</h2>
+                                    <p>Harvard University / 2015 - 2017</p>
+                                    <p className={style.summary_desc}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy text.</p>
+                                </div>
+                                <div className={style.summary_items_container}>
+                                    <h2>Master in Computer Engineering</h2>
+                                    <p>Harvard University / 2015 - 2017</p>
+                                    <p className={style.summary_desc}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy text.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <div className={style.content}>
+                <div className={`${style.content_container} ${style.container_work}`}>
+                    <div>
+                        <p>asdkjl</p>
+                    </div>
+                </div>
+            </div> */}
         </>
     )
 }
